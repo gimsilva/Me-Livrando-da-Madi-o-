327735c3c8fb80c29960b4dcd770663bcf6c45e9
gimsilva <gyduo1@gmail.com>
2021-11-02 20:37:08 -0300

    Primeiro projeto para Exorcizar a maldição do Hello Wolrd

---
+++ Olá Mundo/Olá Mundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Olá_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!!!");
+            Console.WriteLine("Me Livrei da maldição!");
+            Console.WriteLine("Let's Go C#");
+        }
+    }
+}
